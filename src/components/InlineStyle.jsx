30d73958a4fe1e01e71b6e559f55084f796c2650
hideakiyamamoto <61575280+ideahy@users.noreply.github.com>
2021-06-27 09:03:32 +0900

    create InlineStyle

---
+++ src/components/InlineStyle.jsx
@@ -0,0 +1,12 @@
+export const InlineStyle = () => {
+  const style = {
+    backgroundColor: "#ff1100"
+  };
+
+  return (
+    <div style={style}>
+      <p style={style}>- Inline Styles -</p>
+      <button style={style}>FIGHT!!</button>
+    </div>
+  );
+};
